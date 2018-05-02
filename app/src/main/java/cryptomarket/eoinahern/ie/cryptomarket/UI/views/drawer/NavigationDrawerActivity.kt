0cac52676fe3fde69eb4e9393094b15e4141a0f0
eoinahern <eoin.ahern@mycit.ie>
2018-05-02 19:03:00 +0100

    started adding drawer. xml is a pain

---
+++ app/src/main/java/cryptomarket/eoinahern/ie/cryptomarket/UI/views/drawer/NavigationDrawerActivity.kt
@@ -0,0 +1,27 @@
+package cryptomarket.eoinahern.ie.cryptomarket.UI.views.drawer
+
+import android.os.Bundle
+import cryptomarket.eoinahern.ie.cryptomarket.UI.base.BaseActivity
+
+open abstract class NavigationDrawerActivity : BaseActivity() {
+
+
+
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+
+		setListeners()
+	}
+
+
+	private fun setListeners() {
+
+
+
+	}
+
+
+
+}
