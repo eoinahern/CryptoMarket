70a9bf3be4aadc58e7caf3681d302bac14eabc57
eoinahern <eoin.ahern@mycit.ie>
2018-06-06 13:15:54 +0100

    updated test

--- app/src/test/java/cryptomarket/eoinahern/ie/cryptomarket/domain/main/GetCryptoListInteractorTest.kt
+++ app/src/test/java/cryptomarket/eoinahern/ie/cryptomarket/domain/main/GetCryptoListInteractorTest.kt
@@ -7,7 +7,6 @@ import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.*
@@ -20,23 +19,23 @@ class GetCryptoListInteractorTest {
 
 	@Mock
 	lateinit var mockCryptoApi: CryptoApi
-	var currencyData : CurrencyData? = null
+	var currencyData: CurrencyData? = null
 
 	@Mock
 	lateinit var mockCurrData: CurrencyData
 
 	@Mock
-	lateinit var mockFullPriceWrapper : FullPriceWrapper
+	lateinit var mockFullPriceWrapper: FullPriceWrapper
 	@Mock
-	lateinit var mockFullPriceWrapperInternalBTC : FullPriceWrapperInternalDisplay
+	lateinit var mockFullPriceWrapperInternalBTC: FullPriceWrapperInternalDisplay
 	@Mock
-	lateinit var mockFullPriceWrapperInternalETH : FullPriceWrapperInternalDisplay
+	lateinit var mockFullPriceWrapperInternalETH: FullPriceWrapperInternalDisplay
 	@Mock
-	lateinit var mockFullPriceWrapperInternalLTC : FullPriceWrapperInternalDisplay
+	lateinit var mockFullPriceWrapperInternalLTC: FullPriceWrapperInternalDisplay
 	@Mock
 	lateinit var mockFullPriceWrapperInternalDASH: FullPriceWrapperInternalDisplay
 	@Mock
-	lateinit var mockMapInternal : LinkedHashMap<String, FullPriceWrapperInternalDisplay>
+	lateinit var mockMapInternal: LinkedHashMap<String, FullPriceWrapperInternalDisplay>
 
 
 	lateinit var cryptoInteractor: GetCryptoListInteractor
@@ -46,14 +45,14 @@ class GetCryptoListInteractorTest {
 		MockitoAnnotations.initMocks(this)
 
 		Mockito.`when`(mockCryptoApi.getList()).thenReturn(Observable.just(getDataHelper()))
-		cryptoInteractor = GetCryptoListInteractor(mockCryptoApi).setStartLimit(0,4)
+		cryptoInteractor = GetCryptoListInteractor(mockCryptoApi).setStartLimit(0, 4)
 	}
 
-    //test is a bit verbose.
+	//test is a bit verbose.
 	@Test
 	fun buildObservableTest() {
 
-		`when`(mockCryptoApi.getFullPriceData(anyString() , anyString())).thenReturn(Observable.just(mockFullPriceWrapper))
+		`when`(mockCryptoApi.getFullPriceData(anyString(), anyString())).thenReturn(Observable.just(mockFullPriceWrapper))
 		`when`(mockFullPriceWrapper.DISPLAY).thenReturn(mockMapInternal)
 		`when`(mockMapInternal.contains(anyString())).thenReturn(true)
 		`when`(mockMapInternal["BTC"]).thenReturn(mockFullPriceWrapperInternalBTC)
@@ -65,27 +64,30 @@ class GetCryptoListInteractorTest {
 		`when`(mockFullPriceWrapperInternalLTC.item).thenReturn(mutableMapOf("LTC" to CurrencyFullPriceDataDisplay("LTC", "1.00", "1")))
 		`when`(mockFullPriceWrapperInternalDASH.item).thenReturn(mutableMapOf("DASH" to CurrencyFullPriceDataDisplay("DASH", "1.00", "1")))
 
-		val list = cryptoInteractor.buildObservable().blockingFirst()
+		var list = cryptoInteractor.buildObservable().blockingFirst()
 
 		assertEquals(4, list.size)
-		assertEquals("BTC",list[0]?.first?.Symbol)
-		assertEquals("ETH",list[1]?.first?.Symbol)
+		assertEquals("BTC", list[0]?.first?.Symbol)
+		assertEquals("ETH", list[1]?.first?.Symbol)
+
+		 var newlist = cryptoInteractor.buildObservable().blockingFirst()
+		assertEquals(4, newlist.size)
 
 
-		verify(mockCryptoApi).getList()
-		verify(mockCryptoApi).getFullPriceData( "BTC,ETH,LTC,DASH", "EUR,USD,BTC,PLN,GBP")
+		verify(mockCryptoApi, times(2)).getList()
+		verify(mockCryptoApi, times(2)).getFullPriceData("BTC,ETH,LTC,DASH", "EUR,USD,BTC,PLN,GBP")
 
 	}
 
-	private fun getDataHelper() : CurrencyData? {
+	private fun getDataHelper(): CurrencyData? {
 
 		val cryptoBase = CryptoCurrency("1", "url", "url", "bitcoin",
-				"BTC","bitcoin", "bitcoin", "1")
+				"BTC", "bitcoin", "bitcoin", "1")
 
 		val cryptoEth = cryptoBase.copy(Symbol = "ETH", SortOrder = "2")
-        val cryptMon = cryptoBase.copy(Symbol = "LTC", SortOrder = "3")
-        val dash = cryptoBase.copy(Symbol = "DASH", SortOrder = "4")
-		val cryptoMap = HashMap<String , CryptoCurrency>()
+		val cryptMon = cryptoBase.copy(Symbol = "LTC", SortOrder = "3")
+		val dash = cryptoBase.copy(Symbol = "DASH", SortOrder = "4")
+		val cryptoMap = HashMap<String, CryptoCurrency>()
 
 		cryptoMap[cryptoBase.Symbol] = cryptoBase
 		cryptoMap[cryptoEth.Symbol] = cryptoEth
