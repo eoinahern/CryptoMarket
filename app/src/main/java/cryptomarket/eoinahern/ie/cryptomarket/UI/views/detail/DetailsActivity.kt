0ab4d64424d084b0536bfa9a31b51de80ac9e915
eoinahern <eoin.ahern@mycit.ie>
2018-06-15 11:59:36 +0100

    embarrassing cant get backnavigation bug fixed. perplexing

--- app/src/main/java/cryptomarket/eoinahern/ie/cryptomarket/UI/views/detail/DetailsActivity.kt
+++ app/src/main/java/cryptomarket/eoinahern/ie/cryptomarket/UI/views/detail/DetailsActivity.kt
@@ -1,32 +1,50 @@
 package cryptomarket.eoinahern.ie.cryptomarket.UI.views.detail
 
-
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import cryptomarket.eoinahern.ie.cryptomarket.R
 import cryptomarket.eoinahern.ie.cryptomarket.UI.base.BaseActivity
 
 class DetailsActivity : BaseActivity(), DetailsView {
 
-	val detailsToolbar : Toolbar by lazy { findViewById<Toolbar>(R.id.detailsToolbar)}
+	val detailsToolbar: Toolbar by lazy { findViewById<Toolbar>(R.id.detailsToolbar) }
+	var actionBar: ActionBar? = null
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_details)
 
-		setSupportActionBar(detailsToolbar)
-		supportActionBar?.title = null
-		supportActionBar?.setDisplayHomeAsUpEnabled(true)
-		supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_icon)
-		detailsToolbar.setNavigationOnClickListener { _ ->  onBackPressed()}
+		this.setSupportActionBar(detailsToolbar)
+
+		actionBar = supportActionBar?.apply {
+
+			this.title = "BTC"
+			this.setDisplayHomeAsUpEnabled(true)
+			this.setHomeButtonEnabled(true)
+			this.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp)
+		}
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+		return when (item.itemId) {
+			android.R.id.home -> {
+				this.finish()
+				true
+			}
+			else -> super.onOptionsItemSelected(item)
+		}
 	}
 
 	override fun inject() {
 		//not used yet
 	}
 
-	override fun getLayoutView() : Int = R.layout.activity_details
+	override fun getLayoutView(): Int = R.layout.activity_details
 
 	companion object {
 		fun getStartIntent(context: Context): Intent = Intent(context, DetailsActivity::class.java)
