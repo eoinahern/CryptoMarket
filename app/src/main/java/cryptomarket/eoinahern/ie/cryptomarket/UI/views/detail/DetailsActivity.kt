052b9bdaf7d24303b2796f6d0417b7984fe9f78f
eoinahern <eoin.ahern@mycit.ie>
2018-06-16 10:41:54 +0100

    fixed back issue. xml issue

--- app/src/main/java/cryptomarket/eoinahern/ie/cryptomarket/UI/views/detail/DetailsActivity.kt
+++ app/src/main/java/cryptomarket/eoinahern/ie/cryptomarket/UI/views/detail/DetailsActivity.kt
@@ -5,46 +5,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.ActionBar
 import android.support.v7.widget.Toolbar
-import android.view.MenuItem
 import cryptomarket.eoinahern.ie.cryptomarket.R
 import cryptomarket.eoinahern.ie.cryptomarket.UI.base.BaseActivity
 
 class DetailsActivity : BaseActivity(), DetailsView {
 
-	val detailsToolbar: Toolbar by lazy { findViewById<Toolbar>(R.id.detailsToolbar) }
-	var actionBar: ActionBar? = null
+	private val detailsToolbar: Toolbar by lazy { findViewById<Toolbar>(R.id.toolbar) }
+	private var actionBar: ActionBar? = null
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_details)
 
 		this.setSupportActionBar(detailsToolbar)
-
-		actionBar = supportActionBar?.apply {
+		actionBar = supportActionBar
+		actionBar?.apply {
 
 			this.title = "BTC"
 			this.setDisplayHomeAsUpEnabled(true)
 			this.setHomeButtonEnabled(true)
-			this.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp)
+			this.setHomeAsUpIndicator(R.drawable.ic_back_dark)
 		}
-	}
 
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-		return when (item.itemId) {
-			android.R.id.home -> {
-				this.finish()
-				true
-			}
-			else -> super.onOptionsItemSelected(item)
-		}
+		detailsToolbar.setNavigationOnClickListener { _ -> finish() }
 	}
 
 	override fun inject() {
 		//not used yet
 	}
 
-	override fun getLayoutView(): Int = R.layout.activity_details
+	override fun getLayoutView(): Int {
+		return R.layout.activity_details
+	}
 
 	companion object {
 		fun getStartIntent(context: Context): Intent = Intent(context, DetailsActivity::class.java)
@@ -65,4 +56,5 @@ class DetailsActivity : BaseActivity(), DetailsView {
 	override fun DisplayLoadedData() {
 
 	}
+
 }
