82ebc757741f93ef1514df8ea7475716bb1ddbc7
eoinahern <eoin.ahern@mycit.ie>
2018-07-05 21:47:02 +0100

    added gesture detection on Graph. currently testing capabilities

--- app/src/main/java/cryptomarket/eoinahern/ie/cryptomarket/UI/views/detail/DetailsActivity.kt
+++ app/src/main/java/cryptomarket/eoinahern/ie/cryptomarket/UI/views/detail/DetailsActivity.kt
@@ -6,11 +6,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.MotionEvent
+import android.view.MotionEvent.AXIS_X
+import android.view.MotionEvent.AXIS_Y
 import android.view.View
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.ChartTouchListener
+import com.github.mikephil.charting.listener.OnChartGestureListener
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import cryptomarket.eoinahern.ie.cryptomarket.MyApp
 import cryptomarket.eoinahern.ie.cryptomarket.R
 import cryptomarket.eoinahern.ie.cryptomarket.UI.base.BaseActivity
@@ -22,7 +31,7 @@ import cryptomarket.eoinahern.ie.cryptomarket.data.models.HistoricalData
 import kotlinx.android.synthetic.main.activity_details.*
 import javax.inject.Inject
 
-class DetailsActivity : BaseActivity(), DetailsView {
+class DetailsActivity : BaseActivity(), DetailsView, OnChartGestureListener, OnChartValueSelectedListener {
 
 	@Inject
 	lateinit var presenter: DetailsActivityPresenter
@@ -101,6 +110,8 @@ class DetailsActivity : BaseActivity(), DetailsView {
 	private fun setUpGraph() {
 
 		lineGraph.setDrawBorders(false)
+		lineGraph.onChartGestureListener = this
+		lineGraph.setOnChartValueSelectedListener(this)
 		lineGraph.xAxis.setDrawGridLines(false)
 		lineGraph.setDrawBorders(false)
 		lineGraph.xAxis.setDrawLabels(false)
@@ -117,9 +128,9 @@ class DetailsActivity : BaseActivity(), DetailsView {
 	}
 
 	@SuppressLint("ResourceType")
-	private fun addDataSetStyling(dataset: LineDataSet?) {
+	private fun addDataSetStyling(dataSet: LineDataSet?) {
 
-		dataset?.apply {
+		dataSet?.apply {
 			color = ContextCompat.getColor(applicationContext, R.color.mint_green_heavy)
 			lineWidth = 4f
 			setDrawValues(false)
@@ -161,6 +172,54 @@ class DetailsActivity : BaseActivity(), DetailsView {
 	override fun onDestroy() {
 		super.onDestroy()
 		presenter.detachView()
+	}
+
+	/**
+	 * series of listener methods to determine chart crosshairs location etc.
+	 */
+
+	override fun onChartGestureEnd(me: MotionEvent?, lastPerformedGesture: ChartTouchListener.ChartGesture?) {
+		Log.d("gestureEnd", "gestureEnd")
+		//println(me?.rawX)
+		//println(me?.rawY)
+	}
+
+	override fun onChartFling(me1: MotionEvent?, me2: MotionEvent?, velocityX: Float, velocityY: Float) {
+	}
+
+	override fun onChartSingleTapped(me: MotionEvent?) {
+
+	}
+
+	override fun onChartGestureStart(me: MotionEvent?, lastPerformedGesture: ChartTouchListener.ChartGesture?) {
+		Log.d("gestureStart", "start")
+		//println(me?.rawX)
+		//println(me?.rawY)
+	}
+
+	override fun onChartScale(me: MotionEvent?, scaleX: Float, scaleY: Float) {
+	}
+
+	override fun onChartLongPressed(me: MotionEvent?) {
+		Log.d("onChartTranslate", "onCHartTranslate")
+	}
+
+	override fun onChartDoubleTapped(me: MotionEvent?) {
+		Log.d("onChartTranslate", "onCHartTranslate")
+	}
+
+	override fun onChartTranslate(me: MotionEvent?, dX: Float, dY: Float) {
+		Log.d("onChartTranslate", "onCHartTranslate")
+	}
+
+	override fun onNothingSelected() {
+
+	}
+
+	override fun onValueSelected(e: Entry?, h: Highlight?) {
+		Log.d("onChartTranslate", "onCHartTranslate")
+		println(e?.x)
+		println(e?.y)
 	}
 
 }
